b019e234d0242a42e138a33fcec6d054a20a4681
Konstantin Arakantsev <kostya3312@gmail.com>
2019-10-23 21:53:37 +0300

    feat: add ListGroup component to render genres

--- src/services/fakeGenreService.js
+++ src/services/fakeGenreService.js
@@ -1,9 +1,16 @@
+export const ALL_GENRES_ID = 0;
+
 export const genres = [
   { _id: '5b21ca3eeb7f6fbccd471818', name: 'Action' },
   { _id: '5b21ca3eeb7f6fbccd471814', name: 'Comedy' },
   { _id: '5b21ca3eeb7f6fbccd471820', name: 'Thriller' },
 ];
 
-export function getGenres() {
-  return genres.filter(g => g);
+export async function getGenres() {
+  const result = await new Promise(res => {
+    setTimeout(() => {
+      res(genres);
+    }, 1);
+  });
+  return result;
 }
