eca120d7c2d0701e53665c072fda350ce63bf683
Konstantin Arakantsev <kostya3312@gmail.com>
2019-10-25 22:57:00 +0300

    refactor(table): extract table body to separate component

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -3,8 +3,10 @@
 /* eslint-disable react/prop-types */
 
 import React from 'react';
-import Like from './Like';
+
 import TableHeader from './TableHeader';
+import TableBody from './TableBody';
+import Like from './Like';
 
 function MoviesTable({ movies, sortColumn, onDelete, onLike, onSort }) {
   const columns = [
@@ -12,35 +14,30 @@ function MoviesTable({ movies, sortColumn, onDelete, onLike, onSort }) {
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
-    { key: 'like' }, // for Like component
-    { key: 'delete' }, // for Delete button
+    {
+      key: 'like',
+      content: movie => (
+        <Like liked={movie.liked} onLike={() => onLike(movie)} />
+      ),
+    },
+    {
+      key: 'delete',
+      content: movie => (
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={() => onDelete(movie)}
+        >
+          Delete
+        </button>
+      ),
+    },
   ];
 
   return (
     <table className="table">
       <TableHeader columns={columns} onSort={onSort} sortColumn={sortColumn} />
-      <tbody>
-        {movies.map(movie => (
-          <tr key={movie._id}>
-            <td>{movie.title}</td>
-            <td>{movie.genre.name}</td>
-            <td>{movie.numberInStock}</td>
-            <td>{movie.dailyRentalRate}</td>
-            <td>
-              <Like liked={movie.liked} onLike={() => onLike(movie)} />
-            </td>
-            <td>
-              <button
-                type="button"
-                className="btn btn-danger"
-                onClick={() => onDelete(movie)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
+      <TableBody columns={columns} items={movies} />
     </table>
   );
 }
